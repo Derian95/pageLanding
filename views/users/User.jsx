cd9f9b5b1aa2b6af07c773a365f7c529c42d6df4
Derian <dfhamezquita@gmail.com>
2022-05-09 15:51:56 -0500

    add imgs

--- views/users/User.jsx
+++ views/users/User.jsx
@@ -6,9 +6,9 @@ export default function User() {
   return (
     <div className={styles.container} id="client">
         <section>
-        <CardUser image={'../../assets/users/user1.jpg'} title={"nais"}/>
-        <CardUser image={'../../assets/users/user2.jpg'} title={"nais"}/>
-        <CardUser image={'../../assets/users/user3.jpg'} title={"nais"}/>
+        <CardUser image={'../../assets/user1.jpg'} title={"nais"}/>
+        <CardUser image={'../../assets/user2.jpg'} title={"nais"}/>
+        <CardUser image={'../../assets/user3.jpg'} title={"nais"}/>
         </section>
         
 
